e65384c43c5eec244f6c7148e9a6d92fd3e1c1fe
Kartick Jana <kartickjana020@gmail.com>
2023-08-27 13:07:32 +0000

    all test cases passed

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,57 @@
+// Write your code here
+import {Component} from 'react'
+
+import DenominationItem from '../DenominationItem'
+
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {
+    bal: 2000,
+  }
+
+  updateBalance = value => {
+    this.setState(prevState => ({bal: prevState.bal - value}))
+  }
+
+  render() {
+    const {denominationsList} = this.props
+    const {bal} = this.state
+    const name = 'Sarah Williams'
+    const initial = name.slice(0, 1)
+
+    return (
+      <div className="app-container">
+        <div className="cash-withdrawal-container">
+          <div className="user-details-container">
+            <div className="initial-container">
+              <p className="initial">{initial}</p>
+            </div>
+            <p className="name">{name}</p>
+          </div>
+          <div className="balance-container">
+            <p className="your-balance">Your Balance</p>
+            <p className="balance">
+              {bal}
+              <br />
+              <span className="currency-type">In Rupees</span>
+            </p>
+          </div>
+          <p className="withdrawl">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denominationDetails={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
